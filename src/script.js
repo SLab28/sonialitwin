80ebef15a80c9b113f8ae8413e7170f1b306d9d3
SLab28 <creative.slab2.8@gmail.com>
2023-07-10 00:09:59 +0200

    Hi and Scroll

--- src/script.js
+++ src/script.js
@@ -21,4 +21,27 @@ window.addEventListener("scroll", function() {
   lastScroll = currentScroll;
 });
 
+/* Smooth scrolling functionality */ // Get all the links with a hash in the href attribute 
+const links = document.querySelectorAll("a[href*='#']"); // Loop through the links 
+for (let link of links) { // Add a click event listener to each link 
+  link.addEventListener("click", function(event) { // Prevent the default behavior of the link 
+    event.preventDefault(); // Get the target element of the link 
+    const target = document.querySelector(this.hash); // Scroll to the target element with a smooth behavior 
+target.scrollIntoView({ behavior: "smooth" }); }); }
+
+/* Parallax effect functionality */
+// Get the simulation element
+const simulation = document.querySelector(".simulation");
+
+// Add a scroll event listener to the window
+window.addEventListener("scroll", function() {
+   // Get the scroll position of the window
+   const scrollPosition = window.pageYOffset;
+
+   // Calculate the offset of the simulation element based on the scroll position
+   const offset = scrollPosition * 0.5;
+
+   // Set the transform property of the simulation element to translate it vertically by the offset
+   simulation.style.transform = `translateY(${offset}px)`;
+});
 
